624cf4002d77b2d853fb918cd264cc3ebd039850
Ade Novid <adenvt@gmail.com>
2021-12-07 00:49:37 +0700

    added: title and meta description

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react'
 import Navbar from './navbar'
+import Head from 'next/head'
 
 export const Layout: FC = ({ children }) => {
   return (
     <div className="w-full h-full min-h-screen bg-retro font-body text-retro-900">
+      <Head>
+        <title>Pokédex - Ade Novid</title>
+        <meta name="description" content="Retro style Pokédex website, built with React and Next js" />
+      </Head>
       <Navbar />
       <main className="max-w-screen-lg p-3 mx-auto">
         {children}
